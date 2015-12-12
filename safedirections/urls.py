e11d8823966703f324d71ee37bff97b9c3410641
Aluizio <aluizio777@gmail.com>
2015-12-12 05:55:53 -0200

    Created the DangerLevel model

--- safedirections/urls.py
+++ safedirections/urls.py
@@ -3,4 +3,6 @@ import views
 
 urlpatterns = patterns('',
     url(r'^$', views.Index.as_view(), name='index'),
+    url(r'^api/$', views.DangerList.as_view(), name='list'),
+    url(r'^api/(?P<pk>\d+)/$', views.DangerDetail.as_view(), name='detail'),
 )
