3b9e0543d3bb2b45182c0c9fd26b73986ba7a25e
Aluizio <aluizio777@gmail.com>
2015-12-12 15:16:47 -0200

    Fixed a bug on put data.

--- safedirections/serializers.py
+++ safedirections/serializers.py
@@ -7,7 +7,13 @@ class DangerLevelSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = DangerLevel
-        fields = ('id', 'latlng', 'danger_level')
+        fields = ('id', 'latlng', 'danger_level', 'danger_zero', 'danger_one', 'danger_two', 'danger_three')
+        extra_kwargs = {
+            'danger_zero': {'write_only': True},
+            'danger_one': {'write_only': True},
+            'danger_two': {'write_only': True},
+            'danger_three': {'write_only': True},
+        }
 
     def get_danger_level(self, obj):
         return obj.calculateDangerLevel()
