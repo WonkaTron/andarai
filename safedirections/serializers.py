e70c358c8da6d95a945295e9c7d102fd985512a5
Aluizio <aluizio777@gmail.com>
2015-12-12 10:19:51 -0200

    Agora o latlng retorna como objeto ao inves de string.

--- safedirections/serializers.py
+++ safedirections/serializers.py
@@ -1,8 +1,8 @@
 from models import DangerLevel
-from rest_framework import  serializers
-
+from rest_framework import serializers
 
 class DangerLevelSerializer(serializers.ModelSerializer):
+
     class Meta:
         model = DangerLevel
         fields = ('id', 'latlng', 'danger_zero', 'danger_one', 'danger_two', 'danger_three')
--- safedirections/views.py
+++ safedirections/views.py
@@ -3,6 +3,7 @@ from django.views.generic import TemplateView
 from rest_framework import generics
 from models import DangerLevel
 from serializers import DangerLevelSerializer
+import json
 # Create your views here
 
 class Index(TemplateView):
@@ -12,8 +13,17 @@ class DangerList(generics.ListCreateAPIView):
     queryset = DangerLevel.objects.all()
     serializer_class = DangerLevelSerializer
 
+    def get(self, request, *args, **kwargs):
+        response = self.list(request, *args, **kwargs)
+        for marker in response.data:
+            marker['latlng'] = json.loads(marker['latlng'])
+        return response
+
 class DangerDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = DangerLevel.objects.all()
     serializer_class = DangerLevelSerializer
 
-
+    def get(self, request, *args, **kwargs):
+        response = self.retrieve(request, *args, **kwargs)
+        response.data['latlng'] = json.loads(response.data['latlng'])
+        return response
